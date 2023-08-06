356935aae1f550f68b8a9d347ea027ae9263e0ed
Koji Ishimoto <koji.ishimoto@gmail.com>
2023-08-06 16:47:21 +0900

    add Lib.hs file

---
+++ haskell/Lib.hs
@@ -0,0 +1,9 @@
+-- Convert Int to list of Int
+digs :: Integral x => x -> [x]
+digs 0 = []
+digs x = digs (x `div` 10) ++ [x `mod` 10]
+
+-- Convert list of Int to one Int
+fromDigits :: [Int] -> Int
+fromDigits = foldl addDigit 0
+  where addDigit num d = 10*num + d
